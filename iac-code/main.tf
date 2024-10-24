819154710eb2f0db02e42715cdcabdcd235c7cb7
Victor <victoralfred32@gmail.com>
2024-10-24 02:28:29 +0200

    Test if the organisation specific variables are applicable to the configuration

--- iac-code/main.tf
+++ iac-code/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">=1"
+  cloud {
+    organization = "Loot_Learn"
+    workspaces {
+      name = "lootlearn-iac"
+    }
+  }
+}
 module "network_module" {
   source = "./network_module"
 }
