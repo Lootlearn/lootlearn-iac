4734772dde855ba9320400c18b2aaa8f6003db24
Victor <victoralfred32@gmail.com>
2024-10-24 14:13:02 +0200

    Added tags to the subnets

--- iac-code/network_module/main.tf
+++ iac-code/network_module/main.tf
@@ -12,20 +12,32 @@ resource "aws_subnet" "loot_learn_public_subnet_1" {
   vpc_id            = aws_vpc.loot_learn_vpc.id
   availability_zone = var.ap-southeast-2a
   cidr_block = var.public_vpc_cidr_1
+  tags = {
+    Name = "Public Subnet 1"
+  }
 }
 resource "aws_subnet" "loot_learn_public_subnet_2" {
   vpc_id            = aws_vpc.loot_learn_vpc.id
   availability_zone = var.ap-southeast-2b
   cidr_block = var.public_vpc_cidr_2
+  tags = {
+    Name = "Public Subnet 2"
+  }
 }
 
 resource "aws_subnet" "loot_learn_private_subnet_1" {
   vpc_id            = aws_vpc.loot_learn_vpc.id
   availability_zone = var.ap-southeast-2a
   cidr_block = var.private_cidr_1
+  tags = {
+    Name = "Private Subnet 1"
+  }
 }
 resource "aws_subnet" "loot_learn_private_subnet_2" {
   vpc_id            = aws_vpc.loot_learn_vpc.id
   availability_zone = var.ap-southeast-2b
   cidr_block = var.private_cidr_2
+  tags = {
+    Name = "Private Subnet 2"
+  }
 }
