580802b3b0d2940782d308fd22b8e08536b6221c
Victor <victoralfred32@gmail.com>
2024-10-25 15:08:40 +0200

    Configired volume mount

--- iac-code/main.tf
+++ iac-code/main.tf
@@ -4,25 +4,27 @@ module "network_module" {
 resource "random_id" "rand" {
   byte_length = 8
 }
-resource "aws_ebs_volume" "ebs_volume" {
+
+# Define the EBS volume
+resource "aws_ebs_volume" "ebs_block" {
   availability_zone = var.ap-southeast-2a
   size              = 30
-  type              = "gp3"
-  tags = {
-    Name = "LootlearnStorage-${random_id.rand.hex}"
-  }
+  type              = "gp2"
 }
+
+# Attach the EBS volume to the EC2 instance
+resource "aws_volume_attachment" "example" {
+  device_name = "/dev/sdf"                  # Device name
+  volume_id   = aws_ebs_volume.ebs_block.id
+  instance_id = aws_instance.loot-learn-media-server.id
+  force_detach = true                       # Force-detach on re-run (optional)
+}
+
 resource "aws_instance" "loot-learn-media-server" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = var.ssh_public_key
   security_groups = [module.network_module.loot-learn-sg_id]
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    volume_id = aws_ebs_volume.ebs_volume.id
-    encrypted   = true
-    delete_on_termination = true
-  }
   volume_tags = {
     Name = format("test_boot-learn-media-server%s", terraform.workspace)
   }
