ec068bf5445a6bbe76a9e30e7cd46478af5fab80
Victor <victoralfred32@gmail.com>
2024-11-06 04:56:52 +0100

    Decommission test server

--- iac-code/main.tf
+++ iac-code/main.tf
@@ -35,28 +35,28 @@ resource "aws_instance" "loot-learn-media-server" {
   }
 }
 
-resource "aws_instance" "loot-learn-test-server" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.ssh_public_key_name
-  vpc_security_group_ids      = [module.network_module.security_group_id]
-  associate_public_ip_address = true
-  availability_zone           = var.ap-southeast-2a
-  subnet_id                   = module.network_module.subnet_id_public_1
-  tags = {
-    Name = "TEST_SERVER"
-  }
-  # Variables to pass to the user_data script
-  user_data = templatefile("./scripts/user_data.tpl", {
-    mongo_username     = var.mongo_username
-    mongo_password     = var.mongo_password
-    key_store_password = var.key_store_password
-    certificate        = var.certificate
-    pem_key            = var.pem_key
-    user               ="/home/ubuntu"
-    setup_script       = file("${path.module}/scripts/setup_script.sh") # Read in script file
-  })
-}
+# resource "aws_instance" "loot-learn-test-server" {
+#   ami                         = var.ami_id
+#   instance_type               = var.instance_type
+#   key_name                    = var.ssh_public_key_name
+#   vpc_security_group_ids      = [module.network_module.security_group_id]
+#   associate_public_ip_address = true
+#   availability_zone           = var.ap-southeast-2a
+#   subnet_id                   = module.network_module.subnet_id_public_1
+#   tags = {
+#     Name = "TEST_SERVER"
+#   }
+#   # Variables to pass to the user_data script
+#   user_data = templatefile("./scripts/user_data.tpl", {
+#     mongo_username     = var.mongo_username
+#     mongo_password     = var.mongo_password
+#     key_store_password = var.key_store_password
+#     certificate        = var.certificate
+#     pem_key            = var.pem_key
+#     user               ="/home/ubuntu"
+#     setup_script       = file("${path.module}/scripts/setup_script.sh") # Read in script file
+#   })
+# }
 
 # resource "aws_lb_target_group_attachment" "loot-prod_target_port_3004" {
 #   target_group_arn = module.network_module.loot_learn_aws_target_group_3004
